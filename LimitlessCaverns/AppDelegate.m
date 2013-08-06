9f786cdb627096c79dcb4831e63109bceed7edcf
Christopher Turney <chris.turney@dropbox.com>
2013-08-05 17:07:37 -0700

    initial commit

--- LimitlessCaverns/AppDelegate.m
+++ LimitlessCaverns/AppDelegate.m
@@ -7,8 +7,53 @@
 //
 
 #import "AppDelegate.h"
+#import "BumpClient.h"
 
 @implementation AppDelegate
+
+- (void) configureBump {
+    // userID is a string that you could use as the user's name, or an ID that is semantic within your environment
+    [BumpClient configureWithAPIKey:@"your_api_key" andUserID:[[UIDevice currentDevice] name]];
+
+    [[BumpClient sharedClient] setMatchBlock:^(BumpChannelID channel) {
+        NSLog(@"Matched with user: %@", [[BumpClient sharedClient] userIDForChannel:channel]);
+        [[BumpClient sharedClient] confirmMatch:YES onChannel:channel];
+    }];
+
+    [[BumpClient sharedClient] setChannelConfirmedBlock:^(BumpChannelID channel) {
+        NSLog(@"Channel with %@ confirmed.", [[BumpClient sharedClient] userIDForChannel:channel]);
+        [[BumpClient sharedClient] sendData:[[NSString stringWithFormat:@"Hello, world!"] dataUsingEncoding:NSUTF8StringEncoding]
+                                  toChannel:channel];
+    }];
+
+    [[BumpClient sharedClient] setDataReceivedBlock:^(BumpChannelID channel, NSData *data) {
+        NSLog(@"Data received from %@: %@",
+              [[BumpClient sharedClient] userIDForChannel:channel],
+              [NSString stringWithCString:[data bytes] encoding:NSUTF8StringEncoding]);
+    }];
+
+
+    // optional callback
+    [[BumpClient sharedClient] setConnectionStateChangedBlock:^(BOOL connected) {
+        if (connected) {
+            NSLog(@"Bump connected...");
+        } else {
+            NSLog(@"Bump disconnected...");
+        }
+    }];
+
+    // optional callback
+    [[BumpClient sharedClient] setBumpEventBlock:^(bump_event event) {
+        switch(event) {
+            case BUMP_EVENT_BUMP:
+                NSLog(@"Bump detected.");
+                break;
+            case BUMP_EVENT_NO_MATCH:
+                NSLog(@"No match.");
+                break;
+        }
+    }];
+}
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
@@ -16,6 +61,7 @@
     // Override point for customization after application launch.
     self.window.backgroundColor = [UIColor whiteColor];
     [self.window makeKeyAndVisible];
+    [self configureBump];
     return YES;
 }
 
