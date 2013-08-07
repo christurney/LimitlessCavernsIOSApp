9e70409b99c1b15a1a25180ffb88a4ba7431e4f9
Christopher Turney <chris.turney@dropbox.com>
2013-08-07 15:53:43 -0700

    leaderboard

--- LimitlessCaverns/LeaderboardViewController.m
+++ LimitlessCaverns/LeaderboardViewController.m
@@ -7,115 +7,125 @@
 //
 
 #import "LeaderboardViewController.h"
+#import "AppDelegate.h"
+#import "AFJSONRequestOperation.h"
+#import "UIImageView+AFNetworking.h"
+
+static NSString *cellIdentifier = @"Cell";
 
 @interface LeaderboardViewController ()
+
+@property (nonatomic, strong) NSMutableArray *leaders;
+@property (nonatomic, strong) UIImage *placeHolderImage;
 
 @end
 
 @implementation LeaderboardViewController
 
-- (id)initWithStyle:(UITableViewStyle)style
+-(UIImage*)placeHolderImage
 {
-    self = [super initWithStyle:style];
-    if (self) {
-        // Custom initialization
-    }
-    return self;
+    if(_placeHolderImage) return _placeHolderImage;
+    
+    _placeHolderImage = [UIImage imageNamed:@"placeholderImage.jpg"];
+    return _placeHolderImage;
 }
 
 - (void)viewDidLoad
 {
     [super viewDidLoad];
+    [self.tableView registerClass:[UITableViewCell class] forCellReuseIdentifier:cellIdentifier];
 
-    // Uncomment the following line to preserve selection between presentations.
-    // self.clearsSelectionOnViewWillAppear = NO;
- 
-    // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-    // self.navigationItem.rightBarButtonItem = self.editButtonItem;
-}
+    self.leaders = [NSMutableArray array];
 
-- (void)didReceiveMemoryWarning
-{
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+    [self.leaders addObject:[NSDictionary dictionaryWithObjectsAndKeys:
+                             @"person A", @"name", @"http://wdfw.wa.gov/conservation/fisher/graphics/fisher_fullbody.jpg", @"image_url", nil]];
+    [self.leaders addObject:[NSDictionary dictionaryWithObjectsAndKeys:
+                             @"person B", @"name", @"http://wdfw.wa.gov/conservation/fisher/graphics/fisher_fullbody.jpg", @"image_url", nil]];
+    [self.leaders addObject:[NSDictionary dictionaryWithObjectsAndKeys:
+                             @"person C", @"name", @"http://wdfw.wa.gov/conservation/fisher/graphics/fisher_fullbody.jpg", @"image_url", nil]];
+    [self.leaders addObject:[NSDictionary dictionaryWithObjectsAndKeys:
+                             @"person D", @"name", @"http://wdfw.wa.gov/conservation/fisher/graphics/fisher_fullbody.jpg", @"image_url", nil]];
+    [self.leaders addObject:[NSDictionary dictionaryWithObjectsAndKeys:
+                             @"person E", @"name", @"http://wdfw.wa.gov/conservation/fisher/graphics/fisher_fullbody.jpg", @"image_url", nil]];
+    [self.leaders addObject:[NSDictionary dictionaryWithObjectsAndKeys:
+                             @"person F", @"name", @"http://wdfw.wa.gov/conservation/fisher/graphics/fisher_fullbody.jpg", @"image_url", nil]];
+    [self.leaders addObject:[NSDictionary dictionaryWithObjectsAndKeys:
+                             @"person G", @"name", @"http://wdfw.wa.gov/conservation/fisher/graphics/fisher_fullbody.jpg", @"image_url", nil]];
+    [self.leaders addObject:[NSDictionary dictionaryWithObjectsAndKeys:
+                             @"person H", @"name", @"http://wdfw.wa.gov/conservation/fisher/graphics/fisher_fullbody.jpg", @"image_url", nil]];
+
+    // TODO - actually make a server request to populate the self.leaders array
+    /*
+    NSURL *url = [NSURL URLWithString:[requestURLString stringByAppendingString:@"/leaderboard"]];
+    NSURLRequest *request = [NSURLRequest requestWithURL:url];
+
+    AFJSONRequestOperation *operation = [AFJSONRequestOperation
+                                         JSONRequestOperationWithRequest:request
+                                         
+                                         success:^(NSURLRequest *request, NSHTTPURLResponse *response, id JSON) {
+                                             // fill self.leaders with the leaderboard info
+                                             for (NSDictionary *dict in JSON) {
+                                                 [self.leaders addObject:dict];
+                                             }
+                                             // reload the data in the table view???
+                                             [self.tableView reloadData];
+                                         }
+                                         
+                                         failure:^(NSURLRequest *request, NSHTTPURLResponse *response, NSError *error, id JSON) {
+                                             UIAlertView *alertView = [[UIAlertView alloc] initWithTitle:@"Error!"
+                                                                                                 message:[JSON valueForKeyPath:@"message"]
+                                                                                                delegate:self
+                                                                                       cancelButtonTitle:@"OK"
+                                                                                       otherButtonTitles:nil];
+                                             [alertView show];
+                                         }];
+    [operation start];
+     */
 }
 
 #pragma mark - Table view data source
 
 - (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView
 {
-#warning Potentially incomplete method implementation.
-    // Return the number of sections.
-    return 0;
+    return 1;
 }
 
 - (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
 {
-#warning Incomplete method implementation.
-    // Return the number of rows in the section.
+    if(section == 0){
+        return [self.leaders count];
+    }
     return 0;
+}
+
+- (NSString *)tableView:(UITableView *)tableView titleForHeaderInSection:(NSInteger)section
+{
+    if(section == 0){
+        return @"Leaderboard";
+    }
+    return nil;
 }
 
 - (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
 {
-    static NSString *CellIdentifier = @"Cell";
-    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:CellIdentifier forIndexPath:indexPath];
-    
-    // Configure the cell...
-    
+    NSInteger numLeaders = [self.leaders count];
+    if(indexPath.section != 0 || indexPath.row >= numLeaders || indexPath.row < 0) return nil;
+
+    NSDictionary *leader = [self.leaders objectAtIndex:indexPath.row];
+    NSString *positionInLeaderboard = [NSString stringWithFormat:@"#%ld - ", (long)indexPath.row + 1];
+
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:cellIdentifier forIndexPath:indexPath];
+    cell.selectionStyle = UITableViewCellSelectionStyleNone;
+    cell.imageView.contentMode = UIViewContentModeScaleAspectFit;
+    cell.textLabel.text = [positionInLeaderboard stringByAppendingString: [leader objectForKey:@"name"]];
+    [cell.imageView setImageWithURL:[NSURL URLWithString:[leader objectForKey:@"image_url"]] placeholderImage:self.placeHolderImage];    
     return cell;
 }
 
-/*
-// Override to support conditional editing of the table view.
-- (BOOL)tableView:(UITableView *)tableView canEditRowAtIndexPath:(NSIndexPath *)indexPath
+- (CGFloat)tableView:(UITableView *)tableView heightForRowAtIndexPath:(NSIndexPath *)indexPath
 {
-    // Return NO if you do not want the specified item to be editable.
-    return YES;
+    return 75.0;
 }
-*/
 
-/*
-// Override to support editing the table view.
-- (void)tableView:(UITableView *)tableView commitEditingStyle:(UITableViewCellEditingStyle)editingStyle forRowAtIndexPath:(NSIndexPath *)indexPath
-{
-    if (editingStyle == UITableViewCellEditingStyleDelete) {
-        // Delete the row from the data source
-        [tableView deleteRowsAtIndexPaths:@[indexPath] withRowAnimation:UITableViewRowAnimationFade];
-    }   
-    else if (editingStyle == UITableViewCellEditingStyleInsert) {
-        // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-    }   
-}
-*/
-
-/*
-// Override to support rearranging the table view.
-- (void)tableView:(UITableView *)tableView moveRowAtIndexPath:(NSIndexPath *)fromIndexPath toIndexPath:(NSIndexPath *)toIndexPath
-{
-}
-*/
-
-/*
-// Override to support conditional rearranging of the table view.
-- (BOOL)tableView:(UITableView *)tableView canMoveRowAtIndexPath:(NSIndexPath *)indexPath
-{
-    // Return NO if you do not want the item to be re-orderable.
-    return YES;
-}
-*/
-
-#pragma mark - Table view delegate
-
-- (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath
-{
-    // Navigation logic may go here. Create and push another view controller.
-    /*
-     <#DetailViewController#> *detailViewController = [[<#DetailViewController#> alloc] initWithNibName:@"<#Nib name#>" bundle:nil];
-     // ...
-     // Pass the selected object to the new view controller.
-     [self.navigationController pushViewController:detailViewController animated:YES];
-     */
-}
 
 @end
