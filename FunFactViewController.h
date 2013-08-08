51431bc0f44af0d7059d141c076a9248c8257311
Christopher Turney <chris.turney@dropbox.com>
2013-08-07 17:14:19 -0700

    initial fun fact entry view controller

---
+++ FunFactViewController.h
@@ -0,0 +1,13 @@
+//
+//  FunFactViewController.h
+//  LimitlessCaverns
+//
+//  Created by Christopher Turney on 8/7/13.
+//  Copyright (c) 2013 Dropbox. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface FunFactViewController : UIViewController
+
+@end
