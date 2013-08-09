d3d3f26d5bef54a20430577255d1039c9f8ace11
Matthew Jaffe <matthew.j.jaffe@gmail.com>
2013-08-08 22:17:17 -0700

    Ensure table is positioned correctly in iOS7

--- LimitlessCaverns/GuessWhoHalpersViewController.m
+++ LimitlessCaverns/GuessWhoHalpersViewController.m
@@ -69,6 +69,9 @@
     if (self){
         self.funFacts = dictionary[@"facts"];
         self.userDataDictionary = dictionary;
+        if ([self respondsToSelector:@selector(automaticallyAdjustsScrollViewInsets)]){
+            self.tableView.contentInset = UIEdgeInsetsMake(20, 0, 0, 0);
+        }
     }
     return self;
 }
