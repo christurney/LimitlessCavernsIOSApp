b3abf2a67f1eb71ef761caf169571ff1daf930df
Christopher Turney <chris.turney@dropbox.com>
2013-08-08 22:27:20 -0700

    fixing up fun facts editing screen

--- LimitlessCaverns/FunFactViewController.m
+++ LimitlessCaverns/FunFactViewController.m
@@ -31,9 +31,9 @@ static NSString *cellIdentifier = @"Cell";
 {
     self = [super init];
     if (self){
-        self.navigationItem.rightBarButtonItem = [[UIBarButtonItem alloc] initWithTitle:@"Cancel" style:UIBarButtonItemStyleBordered target:self action:@selector(close)];
-        self.navigationItem.leftBarButtonItem = [[UIBarButtonItem alloc] initWithTitle:@"Submit" style:UIBarButtonItemStyleBordered target:self action:@selector(done)];
-        self.title = @"Manage facts";
+        self.navigationItem.leftBarButtonItem = [[UIBarButtonItem alloc] initWithTitle:@"Cancel" style:UIBarButtonItemStyleBordered target:self action:@selector(close)];
+        self.navigationItem.rightBarButtonItem = [[UIBarButtonItem alloc] initWithTitle:@"Done" style:UIBarButtonItemStyleBordered target:self action:@selector(done)];
+        self.title = @"Manage your facts";
     }
     return self;
 }
@@ -56,17 +56,21 @@ static NSString *cellIdentifier = @"Cell";
     self.activityIndicator.autoresizingMask = UIViewAutoresizingFlexibleLeftMargin|UIViewAutoresizingFlexibleRightMargin|UIViewAutoresizingFlexibleTopMargin|UIViewAutoresizingFlexibleBottomMargin;
 
     self.view.backgroundColor = [UIColor whiteColor];
-    int imageBuffer = 30;
+
+    CGFloat buffer = 30.0;
+    CGFloat corderRadius = 5.0;
+    CGFloat borderWidth = 2.0;
+    UIColor *borderColor = [[UIColor grayColor] colorWithAlphaComponent:0.5];
 
     // directionsLabel label
     UILabel *directionsLabel = [[UILabel alloc] initWithFrame:CGRectMake(0,
                                                                          0,
-                                                                         self.view.width - 2 * imageBuffer,
-                                                                         45)];
+                                                                         self.view.width,
+                                                                         30)];
     directionsLabel.centerY = self.view.height*.07;
-    directionsLabel.centerX = self.view.width / 2.0;
-    [directionsLabel setNumberOfLines:2];
-    [directionsLabel setText:@"Select facts in list to edit, swipe across facts in list to delete, or add new facts:"];
+    directionsLabel.centerX = self.view.centerX;
+    [directionsLabel setNumberOfLines:1];
+    [directionsLabel setText:@"Edit facts in list or type new ones:"];
     [directionsLabel setFont:[UIFont boldSystemFontOfSize:[UIFont systemFontSize]]];
     [directionsLabel setTextAlignment:NSTextAlignmentCenter];
     [self.view addSubview:directionsLabel];
@@ -74,54 +78,30 @@ static NSString *cellIdentifier = @"Cell";
     // fun fact entry field
     self.funFactEntryField = [[UITextView alloc] initWithFrame:CGRectMake(0,
                                                                           CGRectGetMaxY(directionsLabel.frame) + 15,
-                                                                          self.view.width - 2 * imageBuffer,
+                                                                          self.view.width - 2 * buffer,
                                                                           80)];
-    self.funFactEntryField.centerX = self.view.width / 2.0;
-
+    self.funFactEntryField.centerX = self.view.centerX;
     self.funFactEntryField.delegate = self;
     self.funFactEntryField.font = [UIFont boldSystemFontOfSize:[UIFont systemFontSize]];
-    self.funFactEntryField.layer.cornerRadius = 5;
-    [self.funFactEntryField.layer setBorderColor:[[[UIColor grayColor] colorWithAlphaComponent:0.5] CGColor]];
-    [self.funFactEntryField.layer setBorderWidth:2.0];
+    self.funFactEntryField.layer.cornerRadius = corderRadius;
+    [self.funFactEntryField.layer setBorderColor:[borderColor CGColor]];
+    [self.funFactEntryField.layer setBorderWidth:borderWidth];
     self.funFactEntryField.clipsToBounds = YES;
     self.funFactEntryField.returnKeyType = UIReturnKeyDone;
     [self.view addSubview:self.funFactEntryField];
 
-    int buttonWidth = 100;
-    int buttonHeight = 40;
-    int buttonOffset = 15;
-
-    // add fact button
-    UIButton *addButton = [UIButton buttonWithType:UIButtonTypeRoundedRect];
-
-    [addButton setFrame:CGRectMake(0,
-                                   CGRectGetMaxY(self.funFactEntryField.frame) + buttonOffset,
-                                   buttonWidth,
-                                   buttonHeight)];
-    addButton.centerX = self.view.centerX;
-
-    [addButton setTitle:@"New Fact" forState:UIControlStateNormal];
-    [addButton addTarget:self
-                  action:@selector(addButtonClicked)
-        forControlEvents:UIControlEventTouchUpInside];
-    [self.view addSubview:addButton];
-
     // table view listing your fun facts
-    self.funFactsTableView = [[UITableView alloc] initWithFrame:CGRectMake(CGRectGetMinX(self.funFactEntryField.frame), CGRectGetMaxY(addButton.frame) + 15, self.funFactEntryField.width, 150) style:UITableViewStylePlain];
+    self.funFactsTableView = [[UITableView alloc] initWithFrame:CGRectMake(CGRectGetMinX(self.funFactEntryField.frame),
+                                                                           CGRectGetMaxY(self.funFactEntryField.frame) + 15,
+                                                                           self.funFactEntryField.width,
+                                                                           220) style:UITableViewStylePlain];
     [self.funFactsTableView registerClass:[UITableViewCell class] forCellReuseIdentifier:cellIdentifier];
     self.funFactsTableView.delegate = self;
     self.funFactsTableView.dataSource = self;
-    self.funFactsTableView.layer.borderColor = [[UIColor redColor] CGColor];
-
-    self.funFactsTableView.layer.cornerRadius = 5;
-    [self.funFactsTableView.layer setBorderColor:[[[UIColor grayColor] colorWithAlphaComponent:0.5] CGColor]];
-    [self.funFactsTableView.layer setBorderWidth:2.0];
+    self.funFactsTableView.layer.borderColor = [borderColor CGColor];
+    self.funFactsTableView.layer.cornerRadius = corderRadius;
     self.funFactsTableView.clipsToBounds = YES;
-
-
-
-    
-    self.funFactsTableView.layer.borderWidth = 3.0;
+    self.funFactsTableView.layer.borderWidth = borderWidth;
     [self.view addSubview:self.funFactsTableView];
 
     // fetch fun facts from the server
@@ -140,7 +120,12 @@ static NSString *cellIdentifier = @"Cell";
                                                  for(NSString *fact in [JSON objectForKey:@"facts"]){
                                                      [self.funFacts addObject:fact];
                                                  }
+                                                 
                                                  [self.funFactsTableView reloadData];
+                                                 if([self.funFacts count] == 0){
+                                                     [self.funFactEntryField becomeFirstResponder];
+                                                 }
+                                                 
                                              }
                                          }
                                          failure:^(NSURLRequest *request, NSHTTPURLResponse *response, NSError *error, id JSON) {
@@ -184,10 +169,27 @@ static NSString *cellIdentifier = @"Cell";
     }
 }
 
+- (BOOL)tableView:(UITableView *)tableView canEditRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    // Return NO if you do not want the specified item to be editable.
+    return indexPath.section == 0 && indexPath.row >= 0 && indexPath.row < [self.funFacts count] && !self.funFactEntryField.isFirstResponder;
+}
+
 // when a row is selected make that fact editable in the textview
 - (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath
 {
     self.funFactEntryField.text = [self.funFacts objectAtIndex:indexPath.row];
+    [self.funFactEntryField becomeFirstResponder];
+}
+
+-(void)textViewDidBeginEditing:(UITextView *)textView
+{
+    self.funFactsTableView.allowsSelection = NO;
+}
+
+-(void)textViewDidEndEditing:(UITextView *)textView
+{
+    self.funFactsTableView.allowsSelection = YES;
 }
 
 - (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
@@ -196,11 +198,10 @@ static NSString *cellIdentifier = @"Cell";
     if(indexPath.section != 0 || indexPath.row >= numFacts || indexPath.row < 0) return nil;
 
     NSString *fact = [self.funFacts objectAtIndex:indexPath.row];
-
     UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:cellIdentifier forIndexPath:indexPath];
     cell.selectionStyle = UITableViewCellSelectionStyleGray;
     cell.textLabel.text = fact;
-    cell.textLabel.font = [UIFont boldSystemFontOfSize:14];
+    cell.textLabel.font = [UIFont boldSystemFontOfSize:[UIFont systemFontSize]];
     return cell;
 }
 
@@ -259,13 +260,6 @@ static NSString *cellIdentifier = @"Cell";
     [operation start];
 }
 
-- (void)addButtonClicked
-{
-    [self.funFactsTableView deselectRowAtIndexPath:[self.funFactsTableView indexPathForSelectedRow] animated:YES];
-    self.funFactEntryField.text = @"";
-    [self.funFactEntryField becomeFirstResponder];
-}
-
 /*
  when the user clicks Done don't treat it as if they typed a newline
  treat it as if they finished editing a fun fact and want to save their changes
@@ -277,26 +271,22 @@ static NSString *cellIdentifier = @"Cell";
         // which row is selected indicates the fun fact they edited
         // if no row is selected then they added a fun fact
         BOOL validFunFact = [[self.funFactEntryField.text stringByTrimmingCharactersInSet:[NSCharacterSet whitespaceCharacterSet]] length] != 0;
-        if(validFunFact)
-        {
-            NSIndexPath *selectedIndexPath = [self.funFactsTableView indexPathForSelectedRow];
+        NSIndexPath *selectedIndexPath = [self.funFactsTableView indexPathForSelectedRow];
 
-            if(!selectedIndexPath){
-                [self.funFacts addObject:self.funFactEntryField.text];
-            } else {
-                [self.funFacts replaceObjectAtIndex:selectedIndexPath.row withObject:self.funFactEntryField.text];
-            }
-            self.funFactEntryField.text = @"";
-            [self.funFactsTableView reloadData];
-            return NO;
-        } else {
-            UIAlertView *alertView = [[UIAlertView alloc] initWithTitle:@"Invalid Fun Fact"
-                                                                message:@"To delete a fun fact, swipe and delete in the fun facts table."
-                                                               delegate:self
-                                                      cancelButtonTitle:@"OK"
-                                                      otherButtonTitles:nil];
-            [alertView show];
+        if(validFunFact && !selectedIndexPath) {
+            [self.funFacts insertObject:self.funFactEntryField.text atIndex:0];
+
+        } else if(validFunFact && selectedIndexPath) {
+            [self.funFacts replaceObjectAtIndex:selectedIndexPath.row withObject:self.funFactEntryField.text];
+
+        } else if(!validFunFact && selectedIndexPath){
+            [self.funFacts removeObjectAtIndex:selectedIndexPath.row];
+
         }
+
+        self.funFactEntryField.text = @"";
+        [self.funFactsTableView reloadData];
+        return NO;
     }
     return YES;
 }
